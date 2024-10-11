6e488b141d5c637c352c8ca5396a3fdb9549085f
Wojciech Kozioł <wojtek.koziol.pl@gmail.com>
2024-10-11 15:53:33 +0200

    Initial Commit

---
+++ CreativeMinds/CreativeMindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreativeMindsApp.swift
+//  CreativeMinds
+//
+//  Created by Wojciech Kozioł on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CreativeMindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
